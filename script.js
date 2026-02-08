f3f02b0b95ade05573d1395125aa2b0f0f46bde4
abrsabine-sys <abr.sabine@gmail.com>
2026-02-08 17:00:23 -0500

    Added card interactivity

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
 console.log("Skill Swap homepage loaded.");
+
 function showInfo(skill) {
     alert("You clicked on: " + skill);
 }
+
+
+const cards = document.querySelectorAll('.card');
+
+cards.forEach(card => {
+    card.addEventListener('click', () => {
+        const skillTitle = card.querySelector('h3').textContent;
+        showInfo(skillTitle);
+    });
+});
+
+cards.forEach(card => {
+    card.addEventListener('mouseover', () => {
+        card.style.backgroundColor = '#f0f0f0';
+    });
+    card.addEventListener('mouseout', () => {
+        card.style.backgroundColor = '';
+    });
+});
