aa676eb56aa4d803b9a38b5185cc66cdd0cb7ef0
abrsabine-sys <abr.sabine@gmail.com>
2026-02-15 19:08:28 -0500

    implemented filterSkillsByCategory

---
+++ skillswap-functions.js
@@ -0,0 +1,10 @@
+module.exports = {
+  filterSkillsByCategory,
+  calculateTotalCosts,
+  matchSkillsToUser
+};
+
+function filterSkillsByCategory(skills, category) {
+  if (category === 'All') return skills;
+  return skills.filter(skill => skill.category === category);
+}
