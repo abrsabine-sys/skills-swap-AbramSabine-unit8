2fb726c3720a968a82494a5fc14714093c24032b
abrsabine-sys <abr.sabine@gmail.com>
2026-02-15 19:35:34 -0500

    connect skill filter to UI

--- skillswap-functions.js
+++ skillswap-functions.js
@@ -1,10 +1,11 @@
-module.exports = {
-  filterSkillsByCategory,
-  calculateTotalCosts,
-  matchSkillsToUser
-};
 
 function filterSkillsByCategory(skills, category) {
-  if (category === 'All') return skills;
-  return skills.filter(skill => skill.category === category);
+    if (category === "All") {
+        return skills;
+    }
+    return skills.filter(skill => skill.category === category);
 }
+
+module.exports = {
+    filterSkillsByCategory,
+};
